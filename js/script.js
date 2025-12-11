36dfa89ddc1b20b4aade68a8ea2b455c8da6d401
dianatoti <dianatoti10@gmail.com>
2025-12-11 13:50:08 +0100

    feat: aggiunto card e inserito in pagina

--- js/script.js
+++ js/script.js
@@ -43,8 +43,26 @@ const teamContainer = document.getElementById("team-container");
 
 // chiedo all'api i dati e la gestisco nel then
 axios.get("https://boolean-teachers.github.io/mock/api/members/")
-.then(response => {
+  .then(response => {
 
-  // prendo l'array dei membri
-  const members = response.data;
+    // prendo l'array dei membri
+    const members = response.data;
 
+    // cicliamo l'array per inserire le card
+    members.forEach(member => {
+
+      const card = `
+ <div class="col-12 col-md-6 col-lg-4">
+                <div class="card h-100 text-center p-3">
+                    <img src="${member.img}" class="card-img" alt="${member.name}">
+                    <div class="card-body">
+                        <h5 class="name">${member.name}</h5>
+                        <p class="role">${member.role}</p>
+                        <p class="emaill">${member.email}</p>
+                    </div>
+                </div>
+            </div>
+`;
+      teamContainer.innerHTML += card;
+    });
+  })
